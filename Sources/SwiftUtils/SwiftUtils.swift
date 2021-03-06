a746e84df473cfcef9c7068a62466eac55fd553b
JHorace <jasumihiro@gmail.com>
2021-03-06 15:13:23 -0800

    Implemented a binary search function and tests for that function

--- Sources/SwiftUtils/SwiftUtils.swift
+++ Sources/SwiftUtils/SwiftUtils.swift
@@ -1,3 +1,26 @@
-struct SwiftUtils {
-    var text = "Hello, World!"
+public func binarySearch<T: Comparable>(_ input: Array<T>, _ target: T) -> Int?{
+    var lower = 0;
+    var upper = input.count - 1;
+    
+    while(upper > lower)
+    {
+        let current = (upper + lower) / 2;
+        
+        if(input[current] == target)
+        {
+            return current;
+        }
+        else if(input[current] > target)
+        {
+            upper = current - 1;
+        }
+        else
+        {
+            lower = current + 1;
+        }
+    }
+    
+    return nil;
 }
+
+
--- Tests/SwiftUtilsTests/SwiftUtilsTests.swift
+++ Tests/SwiftUtilsTests/SwiftUtilsTests.swift
@@ -2,14 +2,24 @@ import XCTest
 @testable import SwiftUtils
 
 final class SwiftUtilsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUtils().text, "Hello, World!")
+
+    func testBinarySearch(){
+        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        
+        XCTAssertEqual(binarySearch(input, 1), 0);
+        XCTAssertEqual(binarySearch(input, 2), 1);
+        XCTAssertEqual(binarySearch(input, 3), 2);
+        XCTAssertEqual(binarySearch(input, 4), 3);
+        XCTAssertEqual(binarySearch(input, 5), 4);
+        XCTAssertEqual(binarySearch(input, 6), 5);
+        XCTAssertEqual(binarySearch(input, 7), 6);
+        XCTAssertEqual(binarySearch(input, 8), 7);
+        XCTAssertEqual(binarySearch(input, 9), 8);
+        XCTAssertEqual(binarySearch(input, 10), 9);
+        
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testBinarySearch", testBinarySearch),
     ]
 }
